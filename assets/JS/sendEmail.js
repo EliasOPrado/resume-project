9a77b29d10a734d8c657ecd31a13718c5ff71938
Oliveira <eliaspradoprofessional@outlook.com>
2019-05-04 13:35:33 +0000

    added function sendEmail() that needs to be fixed.

--- assets/JS/sendEmail.js
+++ assets/JS/sendEmail.js
@@ -1,1 +1,13 @@
-console.log("Hello, the documment is proper connected");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "elias", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    }).then(function(response) {
+            console.log("SECCESS", response);
+        }, function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false; //to block from loading a new page without request.
+}
